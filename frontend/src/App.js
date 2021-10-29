a70ba5f52e3bddceb4303a7aa8565a114f4b2137
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2021-10-29 20:59:20 +0300

    login/signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import Navbar from './Components/Navbar/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Home from './Components/Home/Home';
+import DoneeSign from './Components/Donee/Sign';
+import DonorSign from './Components/Donor/Sign';
 
 function App() {
   return (
    <div>
      <Router>
        <Navbar/>
-       <Route to="/" exact component={Home}/>
+       <Route path="/" exact component={Home}/>
+       <Route path="/donee/sign" component={DoneeSign}/>
+       <Route path="/donor/sign" component={DonorSign}/>
      </Router>
      
    </div>
