215d2989e67e5f6cdaa27c58ae5f6fd560f26260
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2021-10-29 07:25:20 +0300

    home page

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.css"
+const Home = () => {
+    return (
+        <div className="container home">
+            <h4 className="mb-4">Helping Hand</h4>
+            <h6 className="mb-4">Make an Impact in Someone's Life Today</h6>
+            <button className="btn btn-outline-info">See Donation Menu</button>
+        </div>
+    )
+}
+export default Home;
